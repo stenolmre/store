efdc723d52bf5eb034ebc95bafba3355918263ef
Sten Olmre <stenolmre@icloud.com>
2021-03-23 23:51:48 +0200

    remove ignored files

--- next.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  env: {
-    MONGO_URI: "mongodb+srv://etreeningud:kuznef-wowse9-tUzgid@etreeningud-dxlxs.mongodb.net/demo_store?retryWrites=true&w=majority",
-    STRIPE_SK_TEST: "sk_test_51I44ydHjVXzHNIY3dWJOlss9u6iYgifDNoFlZkTxrBViV6C8m9SS8AbOPvEnmIaKTJPfCeMKy598evyrKIviDzG800TMHifJAq",
-    STRIPE_PK_TEST: "pk_test_51I44ydHjVXzHNIY3afSkllnuCkqvduQUsziv4enaZRWFp1tQpHoHrDXMu7Zdkb860eJR8twV0WBaJqEnZf67tS9A00L0YuzYXO",
-    JWT_KEY: "asdladjkasdoiad9889asud98asdjioajs398238j2i3oejo23"
-  }
-}
